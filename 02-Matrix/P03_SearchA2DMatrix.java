604dfb1512f861290d5f8003e02c715444d792e5
Santhosh <santhoshmadhavan2002@gmail.com>
2026-08-05 15:38:19 +0530

    Add Search a 2D Matrix solution

---
+++ 02-Matrix/P03_SearchA2DMatrix.java
@@ -0,0 +1,60 @@
+public class P03_SearchA2DMatrix {
+
+    /*
+     * LeetCode: https://leetcode.com/problems/search-a-2d-matrix/
+     *
+     * Problem:
+     * Search for a target value in a sorted 2D matrix.
+     *
+     * Input:
+     * matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+     * target = 3
+     * Output: true
+     *
+     * Input:
+     * matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+     * target = 13
+     * Output: false
+     *
+     * Approach:
+     * Treat the matrix as a sorted 1D array.
+     * Apply Binary Search.
+     * Convert mid to row and column:
+     * row = mid / n
+     * col = mid % n
+     *
+     * Time: O(log(m * n))
+     * Space: O(1)
+     */
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int left = 0;
+        int right = m * n - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            int row = mid / n;
+            int col = mid % n;
+
+            int val = matrix[row][col];
+
+            if (val == target) {
+                return true;
+            }
+
+            if (val < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
